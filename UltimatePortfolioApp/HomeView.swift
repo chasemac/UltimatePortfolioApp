ebe6f63c38ca2fcc32a7fbf362a40db9a99f9ce2
Chase McElroy <ichasem@gmail.com>
2021-01-30 15:13:53 -0600

    Created tab bar controller

---
+++ UltimatePortfolioApp/HomeView.swift
@@ -0,0 +1,29 @@
+//
+//  HomeView.swift
+//  UltimatePortfolioApp
+//
+//  Created by Chase McElroy on 1/30/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @EnvironmentObject var dataController: DataController
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Add Data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleData()
+                }
+            }
+            .navigationTitle("Home")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
