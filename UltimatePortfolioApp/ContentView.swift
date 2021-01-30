602cb7599a9168fbe4f09515f6ad15d75b91d297
Chase McElroy <ichasem@gmail.com>
2021-01-30 12:13:46 -0600

    Initial Commit

---
+++ UltimatePortfolioApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UltimatePortfolioApp
+//
+//  Created by Chase McElroy on 1/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UltimatePortfolioApp/UltimatePortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioAppApp.swift
+//  UltimatePortfolioApp
+//
+//  Created by Chase McElroy on 1/30/21.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
