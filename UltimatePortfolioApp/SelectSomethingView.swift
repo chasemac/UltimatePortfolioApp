987ae727addc291a7eeb5099da2100572f28fcc4
Chase McElroy <ichasem@gmail.com>
2021-01-31 21:13:30 -0600

    Created icons for items in list view. Handled split view controller issue where it looks like the screen isn't loading.

---
+++ UltimatePortfolioApp/SelectSomethingView.swift
@@ -0,0 +1,22 @@
+//
+//  SelectSomethingView.swift
+//  UltimatePortfolioApp
+//
+//  Created by Chase McElroy on 1/31/21.
+//
+
+import SwiftUI
+
+struct SelectSomethingView: View {
+    var body: some View {
+        Text("Please select something from the menu to begin.")
+            .italic()
+            .foregroundColor(.secondary)
+    }
+}
+
+struct SelectSomethingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectSomethingView()
+    }
+}
