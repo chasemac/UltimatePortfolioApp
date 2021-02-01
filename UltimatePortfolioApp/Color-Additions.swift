b54e7e36f5105db8a6b5a9c960c81d6b332b5a74
Chase McElroy <ichasem@gmail.com>
2021-02-01 12:31:22 -0600

    Created home screen

---
+++ UltimatePortfolioApp/Color-Additions.swift
@@ -0,0 +1,14 @@
+//
+//  Color-Additions.swift
+//  UltimatePortfolioApp
+//
+//  Created by Chase McElroy on 2/1/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+
+}
